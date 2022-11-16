a223bec63bf57ca5ca2c67ecd33f42a9788537af
Tatyana Kulakova <117149308+tanyakul22@users.noreply.github.com>
2022-11-16 21:02:12 +1000

    Dz6

---
+++ Exs001/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите элементы(через пробел): ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse); 
+int chek = 0;
+ 
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        chek++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во элементов больше 0: {chek}");
---
+++ Exs002/Program.cs
@@ -0,0 +1,9 @@
+﻿
+int b1 ,b2 , k1 , k2 ;
+b1 = 2;
+k1 = 5;
+b2 = 4;
+k2 = 9;
+double x=(double)(b2-b1)/(k1-k2);
+double y=(double)(k1*x+b1);
+Console.Write("прямые пересекаются в точках"+x +"и"+y);
